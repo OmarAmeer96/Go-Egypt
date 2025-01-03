d2c1fff419d347d2871dfc54df5ffd08005ea7bc
OmarAmeer96 <omar.ameer244@gmail.com>
2025-01-03 03:50:07 +0200

    Testing CI/CD Wokflow in development

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GoEgypt',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
